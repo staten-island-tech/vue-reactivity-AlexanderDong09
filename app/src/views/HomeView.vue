4c0c71d70afcd6ae62ccbdae68eb2b9c9aa6f574
AlexanderDong09 <alexanderdong09@gmail.com>
2025-01-28 12:44:32 -0500

    tailwind was maded

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -5,5 +5,6 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
+    <h1 class="text-3xl font-bold underline text-blue-600">Hello world!</h1>
   </main>
 </template>
