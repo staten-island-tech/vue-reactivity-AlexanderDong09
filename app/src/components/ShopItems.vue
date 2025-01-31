bd5df810226e6471b7dd235fe13b72bcdc97dc5c
AlexanderDong09 <alexanderdong09@gmail.com>
2025-01-31 12:29:58 -0500

    h

---
+++ app/src/components/ShopItems.vue
@@ -0,0 +1,30 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+const shopItems = [
+  {
+    name: 'Portable Mini Black Hole Generator',
+    price: 3,
+    image: '[Insert Image Here]',
+  },
+  {
+    name: 'Portable Mini Black Hole Generator',
+    price: 3,
+    image: '[Insert Image Here]',
+  },
+  {
+    name: 'Portable Mini Black Hole Generator',
+    price: 3,
+    image: '[Insert Image Here]',
+  },
+  {
+    name: 'Portable Mini Black Hole Generator',
+    price: 3,
+    image: '[Insert Image Here]',
+  },
+]
+</script>
+
+<style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,10 +1,23 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
     <TheWelcome />
-    <h1 class="text-3xl font-bold underline text-blue-600">Hello world!</h1>
+    <h1 class="text-5xl font-bold underline text-blue-600">Hello world!</h1>
+    <div class="card bg-base-100 w-96 shadow-xl">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
