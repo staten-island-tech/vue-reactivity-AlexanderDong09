a89fccc96389aa484bce4929607c38daf97f3166
AlexanderDong09 <alexanderdong09@gmail.com>
2025-02-14 12:59:17 -0500

    twas a test

--- app/src/views/FavoritedItems.vue
+++ app/src/views/FavoritedItems.vue
@@ -2,7 +2,6 @@
   <div class="about">
     <h1 class="text-5xl font-bold underline pb-5">Favorite Items!</h1>
     <h1 v-if="!hasItems">You have nothing favorited, go favorite something!</h1>
-    <
   </div>
 </template>
 
@@ -10,7 +9,7 @@
 import { astronomyObjects } from '@/arrays/objects.js'
 
 astronomyObjects.forEach((object) => {
-  if (object.isFavorited) {
+  if (object.isFavorited === false) {
     console.log(object)
   }
 })
